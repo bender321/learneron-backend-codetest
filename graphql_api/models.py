a505bc7361a64fddb1f9259949d60bcb3718e008
bender321 <bender321@example.com>
2022-01-16 18:55:25 +0100

    Upload changes

--- graphql_api/models.py
+++ graphql_api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Actor(models.Model):
+
+    name = models.CharField(max_length=25)
+    date_of_birth = models.DateField(max_length=9)
+
+    def __str__(self):
+        return self.name
+
+
+class Movie(models.Model):
+
+    name = models.CharField(max_length=50)
+    year = models.IntegerField()
+    actors = models.ManyToManyField(Actor)
+
+    def __str__(self):
+        return self.name
+
+
+
--- graphql_api/schema.py
+++ graphql_api/schema.py
@@ -1,8 +1,80 @@
 import graphene
+from typing import List
+from graphene_django import DjangoObjectType
+from graphql_api.models import Movie, Actor
+
+
+class ActorType(DjangoObjectType):
+    class Meta:
+        model = Actor
+        fields = ("id", "name", "date_of_birth")
+
+
+class MovieType(DjangoObjectType):
+    class Meta:
+        model = Movie
+        fields = ("id", "name", "year", "actors")
+
+
+class CreateActor(graphene.Mutation):
+    class Arguments:
+        name = graphene.String()
+        date_of_birth = graphene.Date()
+
+    actor = graphene.Field(ActorType)
+
+    @classmethod
+    def mutate(cls, root, info, name, date_of_birth):
+        actor = Actor(name=name, date_of_birth=date_of_birth)
+        actor.save()
+        return CreateActor(actor=actor)
+
+
+class CreateMovie(graphene.Mutation):
+    class Arguments:
+        name = graphene.String()
+        year = graphene.Int()
+        actors = graphene.List(graphene.String)
+
+    movie = graphene.Field(MovieType)
+
+    @classmethod
+    def mutate(cls, root, info, name, year, actors):
+        movie = Movie(name=name, year=year)
+        movie.save()
+
+        for _ in actors:
+            actor = Actor.objects.get(name=_)
+            movie.actors.add(actor.id)
+
+        return CreateMovie(movie=movie)
 
 
 class Query(graphene.ObjectType):
-    hello = graphene.String(default_value="Hi!")
+
+    all_movies = graphene.List(
+        MovieType,
+        since=graphene.Argument(graphene.Int, required=False, default_value={}),
+        till=graphene.Argument(graphene.Int, required=False, default_value={})
+    )
+
+    def resolve_all_movies(root, info, since, till):
+
+        movies = Movie.objects.all()
+
+        if since == {}:
+            return movies
+        else:
+            res = []
+            for _ in movies:
+                if till >= _.year >= since:
+                    res.append(_)
+            return res
 
 
-schema = graphene.Schema(query=Query)
+class Mutation(graphene.ObjectType):
+    create_actor = CreateActor.Field()
+    create_movie = CreateMovie.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
